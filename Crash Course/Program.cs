e07cf758195665215b901bccbeeb7e52f194d923
jordandonahue <jdonah7053@my.sctd.edu>
2017-01-03 20:44:42 -0500

    Declared, initialized and printed the int, bool, double, char and string variables and printed to the console.

--- Crash Course/Program.cs
+++ Crash Course/Program.cs
@@ -12,7 +12,22 @@ namespace Crash_Course
         {
 
             Console.WriteLine("Hello, World!");
+            
+
+            int integer1 = 10;
+            bool boolean1 = true;
+            double double1 = 1.0;
+            char character1 = 'k';
+            string string1 = "Hello";
+            Console.WriteLine("");
+            Console.WriteLine("integer1 = " + integer1);
+            Console.WriteLine("boolean1 = " + boolean1);
+            Console.WriteLine("double1 = " + double1);
+            Console.WriteLine("character1 = " + character1);
+            Console.WriteLine("string1 = " + string1);
             Console.Read();
+
+
 
         }
     }
