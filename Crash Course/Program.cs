7e3a4de6d45ba74737bdccb3d25473754aa0d07c
jordandonahue <jdonah7053@my.sctd.edu>
2017-01-10 18:19:12 -0500

    Changed code to create a integer based numbers game.

--- Crash Course/Program.cs
+++ Crash Course/Program.cs
@@ -10,41 +10,32 @@ namespace Crash_Course
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Hello, World!");
-            
-
-            int integer1 = 10;
-            bool boolean1 = true;
-            double double1 = 1.0;
-            char character1 = 'k';
-            string string1 = "Hello";
-            Console.WriteLine("");
-            Console.WriteLine("integer1 = " + integer1);
-            Console.WriteLine("boolean1 = " + boolean1);
-            Console.WriteLine("double1 = " + double1);
-            Console.WriteLine("character1 = " + character1);
-            Console.WriteLine("string1 = " + string1);
-            Console.WriteLine("");
-
-            int winningNumber = 2;
-             
-
-            Console.WriteLine("Please enter a number 1-3 to find out if you have won!");
-            string userNumber = Console.ReadLine();
-
-            int userInt;
-            int.TryParse(userNumber, out userInt);
-
-            if (userInt == 2)
+            //give the user directions(1-5)
+            Console.WriteLine("Pick a number (1-5)");
+            //set the winning numbers
+            const int winningNumber1 = 3;
+            const int winningNumber2 = 5;
+            //get data from the user
+            string userData = Console.ReadLine();
+            //convert it to int
+            int userValue;
+            int.TryParse(userData, out userValue);
+            //do they match?
+            switch(userValue)
             {
-                Console.WriteLine("You Won!");
+                case winningNumber1:
+                    Console.WriteLine("You win!");
+                    break;
+                case winningNumber2:
+                    Console.WriteLine("You kinda win!");
+                    break;
+                default:
+                    Console.WriteLine("You definitely lose!");
+                    break;
+
             }
-            else
-            {
-                Console.WriteLine("You Lost!");
-        
-            }
+
+
             Console.Read();
         }
     }
