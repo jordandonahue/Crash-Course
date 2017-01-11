71496d0c7ef3f2f72bee960428df8f89028b1c11
jordandonahue <jdonah7053@my.sctd.edu>
2017-01-10 19:24:14 -0500

    changed code to include a 'fizzbuzz' determination sequence.

--- Crash Course/Program.cs
+++ Crash Course/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,31 +11,111 @@ namespace Crash_Course
     {
         static void Main(string[] args)
         {
-            //give the user directions(1-5)
-            Console.WriteLine("Pick a number (1-5)");
-            //set the winning numbers
-            const int winningNumber1 = 3;
-            const int winningNumber2 = 5;
-            //get data from the user
-            string userData = Console.ReadLine();
-            //convert it to int
-            int userValue;
-            int.TryParse(userData, out userValue);
-            //do they match?
-            switch(userValue)
+            for (int i = 1; i <= 100; i++)
             {
-                case winningNumber1:
-                    Console.WriteLine("You win!");
-                    break;
-                case winningNumber2:
-                    Console.WriteLine("You kinda win!");
-                    break;
-                default:
-                    Console.WriteLine("You definitely lose!");
-                    break;
 
+                bool fizz = i % 3 == 0;
+                bool buzz = i % 5 == 0;
+                if (fizz && buzz)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fizz)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (buzz)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else{ Console.WriteLine(i);
+                }
+           }
+            //for (int i = 1; i <= 100; i++)
+            //{
+
+            //    if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+
+
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+
+            //    }
+            //        Console.WriteLine(i);
+
+            //    }
+            //}
+            
+            
+            
+            
+            
+            //List<string> listOfStuff = new List<string>();
+            //List<int> listOfNumbers = new List<int>();
+            
+            
+            //ArrayList listOfStuff = new ArrayList();
+            //listOfStuff.Add("Thing-0");
+            //listOfStuff.Add("Thing-1");
+            //listOfStuff.Add("Thing-2");
+            //listOfStuff.Add("Thing-3");
+            //listOfStuff.Add("Thing-4");
+
+            //foreach (string thing in listOfStuff)
+            //{
+            //    Console.WriteLine(thing);
+            //}
+
+
+            //Console.WriteLine(listOfStuff.Count);
+
+            //int destroyIndex = listOfStuff.IndexOf("Thing-1");
+            //listOfStuff.RemoveAt(destroyIndex);
+
+            //foreach (string thing in listOfStuff)
+            //{
+            //    Console.WriteLine(thing);
+            //}
+
+            //Console.WriteLine(listOfStuff.Count);
+
+            //string[] arrayOfStuff = new string[5];
+            //arrayOfStuff[0] = "Thing-0";
+            //arrayOfStuff[1] = "Thing-1";
+            //arrayOfStuff[2] = "Thing-2";
+            //arrayOfStuff[3] = "Thing-3";
+            //arrayOfStuff[4] = "Thing-4";
+
+            //for (int i = 0; i < arrayOfStuff.Length; i++) 
+            //{
+            //    Console.WriteLine(arrayOfStuff[i]);
+            //}
+
+
+            //for(int i = 0; i < 5; i++)
+            //{
+            //    Console.WriteLine("Hello World!");
+
+            //}
+
+            //int counter = 5;
+            //do
+            //{
+            //    Console.WriteLine("Hello World!");
+            //    counter--;
+            //}
+            //while (counter > 5);
+
+            /*while(counter > 0)
+            {
+                Console.WriteLine("Hello World!");
+                counter--;
             }
-
+            */
 
             Console.Read();
         }
